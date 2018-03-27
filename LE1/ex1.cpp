fec88a775cd48e85bf32b68c4ddab9e9fd861c2f
Gguidini <giovanni.guidini@gmail.com>
2018-03-27 13:15:19 -0300

    Mudanças radicais no projeto. Faltam uns getchars. Ta funcionando

--- LE1/ex1.cpp
+++ LE1/ex1.cpp
@@ -1,5 +1,5 @@
 /* Estudantes: 
-        Giovanni Guidini    
+        Giovanni Guidini    16/0122660;
         Vitor Fernandes Dullens 16/0148260; 
   Disciplina: 
         Organizacao de Arquivos 1/2018
@@ -23,8 +23,10 @@ using namespace std;
 void menu() { //menu inicial
     system(CLEAR);
     printf("==============================\n");
-    printf(" Concatenador de Arquvios .txt\n");
+    printf(" Concatenador de Arquvios     \n");
     printf("==============================\n");
+
+    printf("\n\n(press ENTER to continue)\n");
     getchar();
 }
 
@@ -49,12 +51,126 @@ int type(){ //como que o usuario deseja concatenar os arquivos
     return type; 
 }
 
+string readFile(fstream file, int mode){
+    int n;
+    if(mode == 1){
+        printf("Ler quantas linhas?\n");
+        cin >> n;
+    }
+    else if(mode == 2){
+        printf("Ler quantas palavras?\n");
+        cin >> n;
+    }
+    else if(mode == 3){
+        printf("Ler quantos caracteres?\n");
+        cin >> n;
+    }
+    else{
+        // arquivo inteiro == numero enorme de linhas
+        n = 100000;
+    }
+    
+    string r = "", line;
+    if(mode == 1 || mode == 4){
+        int i = 0;
+        while(getline(file, line) && i < n){
+            r += line;
+            i++;
+        }
+    }
+    else if(mode == 2){
+        int i = 0;
+        while(i < n){
+            file >> line;
+            r += line;
+            i++;
+        }
+    }
+    else{
+        char read[n+1];
+        file.get(read, n);
+        r += string(read);
+    }
+    
+    return r;
+}
+
+// arquivo de leitura
+fstream openFile(string file){
+    fstream arquivo (file, ios::in);
+    if(!arquivo){
+        cout << "Failed to open\n";
+        return (fstream) NULL;
+    }
+
+    return arquivo;
+    
+
+}
+
+string fileOP(){
+    string file;
+    system(CLEAR);
+    printf("Qual o nome do arquivo?\n");
+    cin >> file;
+    fstream ini = openFile(file);
+    if(!ini){
+        string again;
+        printf("Tentar novamente? [Y/n]\n");
+        cin >> again;
+        if(again == "Y" || again == "y")
+            return fileOP();
+        else
+            return "";
+    }
+    
+    int t = type();
+    return readFile(openFile(file), t);
+}
+
+int continua(){
+    printf("\nO que voce quer fazer agora?\n");
+    printf("\t(1) - Adicionar outro arquivo\n");
+    printf("\t(2) - Mostrar resultados\n");
+    printf("\t(3) - Finalizar operação\n");
+
+    int type;
+    scanf("%d", &type);
+    while(type < 0 or type > 3){
+        printf("--- opcao inválida, escreva uma opcao válida ---\n");
+        printf("opcao: ");
+        scanf("%d", &type);
+    }
+    return type; 
+}
 
 int main(){
-    string file;
-    int t; 
-
     menu();
-    t = type();
+    string file;
+    string content = "";    
+    // arquivo inicial
+    content += fileOP();
+    
+    // menu das operações
+    int go = continua();
+    while(go != 3){
+        if(go == 1){
+            content += fileOP();
+        }
+        else{
+            system(CLEAR);
+            cout << content;
+
+            printf("\n\n(press ENTER to continue)\n");
+            getchar();
+        }
+        go = continua();
+    }
+
+    printf("Qual arquivo voce quer salvar?\n");
+    cin >> file;
+    fstream f (file, ios::out | ios::app);
+    f << content;
+    return 0;
 
 }
