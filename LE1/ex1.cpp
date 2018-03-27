a60569bc02cb65d2b0c177364227f23f26736620
Vitor Dullens <vitordullens2.0@gmail.com>
2018-03-27 10:50:40 -0300

    adding interface

--- LE1/ex1.cpp
+++ LE1/ex1.cpp
@@ -1,8 +1,61 @@
+/* Estudantes: 
+        Giovanni Guidini    
+        Vitor Fernandes Dullens 16/0148260; 
+  Disciplina: 
+        Organizacao de Arquivos 1/2018
+*/
+
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <string>
+#include <map>
+
+//define para limpar a tela
+#ifdef WIN32
+    #define CLEAR "cls"
+#else
+    #define CLEAR "clear"
+#endif
 
 using namespace std;
 
+void menu() { //menu inicial
+    system(CLEAR);
+    printf("==============================\n");
+    printf(" Concatenador de Arquvios .txt\n");
+    printf("==============================\n");
+    getchar();
+}
+
+int qnt() { //funcao para escolher quantidade de arquivos a serem concatenados
+    int qnt;
+    system(CLEAR);
+    printf("Quantos arquivos para concatenar?  ");
+    scanf("%d", &qnt);
+}
+
+int type(){ //como que o usuario deseja concatenar os arquivos
+    int type;
+    printf("Concatenar:\n");
+    printf("\t1 - linha por linha\n");
+    printf("\t2 - palavra por palavra\n");
+    printf("\t3 - char por char\n");
+    printf("opcao: ");
+    scanf("%d", &type);
+    while(type < 0 or type > 3){
+        printf("--- opcao inválida, escreva uma opcao válida ---\n");
+        printf("opcao: ");
+        scanf("%d", &type);
+    } 
+}
+
 int main(){
-    printf("ola mundo!\n");
-    return 0;
+    int t; 
+    int q;
+
+    menu();
+    q = qnt();
+    t = type();
 }
