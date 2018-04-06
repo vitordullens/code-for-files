f01e879bf85c803c232572697d9056a02fda55c1
Vitor Dullens <vitordullens2.0@gmail.com>
2018-04-05 23:55:59 -0300

    adding comments

--- LE3/ex1.cpp
+++ LE3/ex1.cpp
@@ -13,11 +13,11 @@
 
 using namespace std;
 
-char choice;
+char choice; 
 string name, last, address, tel, cep;
 fstream arquivo("out.txt", ios::app);
 
-void question(){
+void question(){ //lendo os dados do usuario conforme pedido
     system(CLEAR);
     cout << "digite seu primeiro nome: ";
     cin >> name;
@@ -35,7 +35,7 @@ void question(){
     cin >> cep;
     system(CLEAR);
 }
-void choose(){
+void choose(){ //escolher atraves de linha de comando o metodo de organizacao
     system(CLEAR);
     cout << "como deseja listar os registros:" << endl;
     cout << "(1) campos de tamanho fixo" << endl;
@@ -43,17 +43,20 @@ void choose(){
     cout << "(3) campos com separadores" << endl;
     cout << "(4) campos com atributo = valor" << endl;
     cin >> choice;
-    while (choice != '1' and choice != '2' and choice != '3' and choice != '4'){ //tratamento de erro
+    while (choice != '1' and choice != '2' and choice != '3' and choice != '4'){ 
+        //tratamento de erro
         printf("--- opcao invalida, escreva uma opcao valida ---\n");
         printf("opcao: ");
         cin >> choice;
     }
 }
-void writeFile(){
+void writeFile(){ //escrever no arquivo conforme o metodo escolhido
     char Char;
 
     if(choice == '1'){
         char nome[15], ultimo[15], end[15], tele[15], zip[15];
+        //setando previamente todos os valores dos char* para '-',
+        //para evitar lixo de memoria
         memset(nome, '-', 16);
         memset(ultimo, '-', 16);
         memset(end, '-', 16);
@@ -61,6 +64,7 @@ void writeFile(){
         memset(zip, '-', 16);
 
         int i;
+        //copiando arquivos das striings lidas e transformando-os em char*
         strcpy(nome, name.c_str());
         strcpy(ultimo, last.c_str());
         strcpy(end, address.c_str());
@@ -72,7 +76,7 @@ void writeFile(){
         for (i = 0; i <= 15; i++) arquivo << tele[i];
         for (i = 0; i <= 15; i++) arquivo << zip[i];
     } 
-    if (choice == '2'){
+    if (choice == '2'){ //utilizando a funcao size() para achar o tamanho da string
         arquivo << name.size() << name;
         arquivo << last.size() << last;
         arquivo << address.size() << address;
@@ -81,6 +85,7 @@ void writeFile(){
     }
     if (choice == '3'){
         cout << "qual caracter sera o separador: ";
+        //eh possivel escolher qual o caracter separador
         cin >> Char;
         arquivo << name << Char;
         arquivo << last << Char;
@@ -94,7 +99,7 @@ void writeFile(){
     }
     arquivo << "\n";
 }
-void showTxt(){
+void showTxt(){ //mostrar ao usuario o resultado do arquivo texto gerado
    
 }
 int main(){
