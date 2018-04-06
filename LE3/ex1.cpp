a97b87c46ed6e0eab72e820b16bed72e2cd79872
Vitor Dullens <vitordullens2.0@gmail.com>
2018-04-05 23:29:27 -0300

    almost done

--- LE3/ex1.cpp
+++ LE3/ex1.cpp
@@ -1,4 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <cstdlib>
 
 #ifdef WIN32
     #define CLEAR "cls"
@@ -8,8 +13,9 @@
 
 using namespace std;
 
-int choice;
+char choice;
 string name, last, address, tel, cep;
+fstream arquivo("test.txt", ios::app);
 
 void question(){
     system(CLEAR);
@@ -19,7 +25,6 @@ void question(){
     cout << "digite seu ultimo nome: ";
     cin >> last;
     system(CLEAR);
-    /*
     cout << "digite seu endereco: ";
     cin >> address;
     system(CLEAR);    
@@ -28,35 +33,69 @@ void question(){
     system(CLEAR);    
     cout << "digite seu cep: ";
     cin >> cep;
-    system(CLEAR);    
-    */
+    system(CLEAR);
 }
 void choose(){
     system(CLEAR);
     cout << "como deseja listar os registros:" << endl;
     cout << "(1) campos de tamanho fixo" << endl;
     cout << "(2) campos de tamanho no inicio" << endl;
-    cout << "(3) campos com separados" << endl;
+    cout << "(3) campos com separadores" << endl;
     cout << "(4) campos com atributo = valor" << endl;
     cin >> choice;
-    while (choice < 0 or choice > 4){ //tratamento de erro
+    while (choice != '1' and choice != '2' and choice != '3' and choice != '4'){ //tratamento de erro
         printf("--- opcao invalida, escreva uma opcao valida ---\n");
         printf("opcao: ");
         cin >> choice;
     }
 }
-void listReg(){
-    if(choice == 1) 
-        cout << "1" << endl;
-    if (choice == 2)
-        cout << "2" << endl;
-    if (choice == 3)
-        cout << "3" << endl;
-    if (choice == 4)
-        cout << "4" << endl;
+void writeFile(){
+    char Char;
+
+    if(choice == '1'){
+        char nome[15], ultimo[15], end[15], tele[15], zip[15];
+        int i;
+        strcpy(nome, name.c_str());
+        strcpy(ultimo, last.c_str());
+        strcpy(end, address.c_str());
+        strcpy(tele, tel.c_str());
+        strcpy(zip, cep.c_str());
+        for (i = 0; i <= 15; i++) arquivo << nome[i];
+        for (i = 0; i <= 15; i++) arquivo << ultimo[i];
+        for (i = 0; i <= 15; i++) arquivo << end[i];
+        for (i = 0; i <= 15; i++) arquivo << tele[i];
+        for (i = 0; i <= 15; i++) arquivo << zip[i];
+    } 
+    if (choice == '2'){
+        arquivo << name.size() << name;
+        arquivo << last.size() << last;
+        arquivo << address.size() << address;
+        arquivo << tel.size() << tel;
+        arquivo << cep.size() << cep;
+    }
+    if (choice == '3'){
+        cout << "qual caracter sera o separador: ";
+        cin >> Char;
+        arquivo << name << Char;
+        arquivo << last << Char;
+        arquivo << address << Char;
+        arquivo << tel << Char;
+        arquivo << cep << Char;
+    }
+    if (choice == '4'){
+        cout << "waat" << endl;
+
+    }
+    arquivo << "\n";
+}
+void showTxt(){
+   
 }
 int main(){
    question();
    choose();
-   listReg();
+   writeFile();
+   system(CLEAR);
+   cout << "--- arquivo out.txt ---" << "\n\n";
+   showTxt();
 }
