c19e15af53b93100952f7a17dd037ca24607f1f8
Gguidini <giovanni.guidini@gmail.com>
2018-04-22 11:36:40 -0300

    Apparently the reading is better now

--- LE4/ex1.cpp
+++ LE4/ex1.cpp
@@ -6,7 +6,7 @@
 // Thorough explanation on DICOM standard: https://www.leadtools.com/help/leadtools/v19/dh/to/di-topics-basicdicomfilestructure.html
 // Data Element Structure: https://www.leadtools.com/help/leadtools/v19/dh/to/di-topics-dataelementstructure.html
 
-// Online viewer for metadatahttps://www.get-metadata.com/
+// Online viewer for metadata https://www.get-metadata.com/
 
 #include <bits/stdc++.h>
 #define lsDcm() std::system("ls *.dcm")
@@ -41,8 +41,18 @@ bool specialVR(char* vr){
     }
     return false;
 }
+
+bool implicitVR(char* vr){
+    if(vr[0] > 90 || vr[0] < 65){
+        return true;
+    }
+    else if(vr[1] > 90 || vr[1] < 65){
+        return true;
+    }
+    return false;
+}
 void readNextMeta(ifstream* fd){
-    char buff[128], vr[2];
+    char buff[128], vr[10];
     uint16_t tag[2], len;
     unsigned long UL;
     fd->read(buff, 4);
@@ -53,13 +63,34 @@ void readNextMeta(ifstream* fd){
     // Dealing with cases OB, OW, OF, SQ, UT or UN for explicit VR, which have 2 reserved bytes
     if (specialVR(vr)){
         // forwards two bytes
+        cout << "Forwarded 2 bytes because of specialVR\n";
         fd->read(buff, 2);
+        int len;
+        fd->read(buff, 4);  // special values have 4 bytes of length
+        len = (int) buff[0];
+        fd->read(buff, len);
+        UL = (unsigned long) buff[0];
     }
+    // Some cases may contain implicit vr (?)
+    else if(implicitVR(vr)){
+        // backwards two bytes
+        cout << "Backwarded 2 bytes because of implicitVR\n";
+        int curr = fd->tellg();
+        fd->seekg(curr-2);
+        strcpy(vr, "Implicit\0");
 
-    fd->read(buff, 2);
-    len = (uint16_t) buff[0];
-    fd->read(buff, len);
-    UL = (unsigned long) buff[0];
+        int len;
+        fd->read(buff, 4);  // special values have 4 bytes of length
+        len = (int) buff[0];
+        fd->read(buff, len);
+        UL = (unsigned long) buff[0];
+    }
+    else{
+        fd->read(buff, 2);
+        len = (uint16_t) buff[0];
+        fd->read(buff, len);
+        UL = (unsigned long) buff[0];
+    }
 
     printf("TAG: (%#x,%#x)\n", tag[0], tag[1]);
     printf("VR: %c%c\n", vr[0], vr[1]);
@@ -81,7 +112,6 @@ int main(){
     }
     
     managePreamble(&fd);
-    int back = (int) fd.tellg();
 
     for(int i = 0; i < 5; i++){
         readNextMeta(&fd);
