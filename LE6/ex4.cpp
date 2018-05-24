1d30f76d3656be1cfdcbb946d1b817a2cec199c6
Vitor Dullens <vitordullens2.0@gmail.com>
2018-05-24 16:50:43 -0300

    blah

--- LE6/ex4.cpp
+++ LE6/ex4.cpp
@@ -8,6 +8,22 @@
 
 using namespace std;
 
+fstream arquivo;
+
+void read(int id){
+    vector<string> data(10);
+    vector<string> question = {"Modelo?", "Marca?", "Placa mae?", "CPU?", "GPU?", "Resolucao?", "RAM?", "Ano de lancamento?", "Tamanho do HD?", "Preco?"};
+        
+    cout << "## Preencha o formulario com base nas informacoes do seu Computador ##" << endl;
+    arquivo << id << "|";
+    for(int i=0;i<10;i++){
+        cout << question[i] << "  ";
+        getline(cin, data[i]) ;
+        arquivo << data[i] << "|";
+    }
+    arquivo << endl;
+
+}
 void menu(){
     system(CLEAR);
     printf("#################################\n");
@@ -25,5 +41,31 @@ void menu(){
 
 int main(){
     menu();
+    int header;
+    time_t time;
+    char traco;
+    string op;
+    bool go = true;
+    
+    arquivo.open("computer.txt", ios::in);
+    arquivo >> header >> traco >> time;
+    arquivo.close();
+    
+    arquivo.open("computer.txt", ios::app);
+
+    while(go){
+        read(++header);
+        cout << "quer digitar outro computador? [y/n]  ";
+        while(true){
+            getline(cin,op);
+            if(op[0] == 'n' or op[0] == 'N'){
+                go = false;
+                break;
+            } 
+            else if(op[0] =='y' or op[0] == 'Y') break;
+            cout << "## digite y ou n apenas:  ";
+        }
+    }
+    arquivo.close();
 
 }
